a5adc6f44b9345d686220ed7dc7b81e595b4e3d5
Braiao <86326087+Braiao@users.noreply.github.com>
2024-05-17 10:26:59 -0300

    actions_att

--- generate.php
+++ generate.php
@@ -48,7 +48,7 @@ foreach ($stations as $station) {
     $version = "-R3"; // Defina a versão desejada ("-R3", "2.11c", ou null)
 
     $i = 0;
-    $files = ['STSH137a00.24_']; // Adicione seus arquivos aqui
+    $files = ['STSH137a00.24_.gz']; // Adicione seus arquivos aqui
     $lenght = count($files);
    // $obj->setReceiverParams();
 
