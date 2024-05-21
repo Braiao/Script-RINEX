7a2081688bb7e60cce66628d7415fd47908c2241
Braiao <86326087+Braiao@users.noreply.github.com>
2024-05-21 10:41:58 -0300

    $teste

--- generate.php
+++ generate.php
@@ -72,7 +72,7 @@ foreach ($stations as $station) {
     $version = "-R3"; // Defina a versão desejada ("-R3", "2.11c", ou null)
 
     $i = 0;
-    $file = $station . "$array_data[yday]" . $letter . $counter . $doisdig_ano . '_.gz';
+    $file = $station . "$array_data[yday]" . "$letter" . $counter . $doisdig_ano . '_.gz';
     //station+diadoano+horapeloalfabeto+minuto(00-15-30-45)+.doiultimosdigitosdoano+_.gz 
     $files = [$file];
     //$files = ['STSH137a00.24_.gz']; // Adicione seus arquivos aqui
