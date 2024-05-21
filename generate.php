a13f13a02cdcbed623030f81258173d95dbf4b54
Braiao <86326087+Braiao@users.noreply.github.com>
2024-05-21 14:44:51 -0300

    $teste

--- generate.php
+++ generate.php
@@ -74,6 +74,7 @@ foreach ($stations as $station) {
     $i = 0;
     $file = $station . "$array_data[yday]" . "$letter" . $counter . $doisdig_ano . '_.gz';
     //station+diadoano+horapeloalfabeto+minuto(00-15-30-45)+.doiultimosdigitosdoano+_.gz 
+    shell_exec('mkdir $file');
     $files = [$file];
     //$files = ['STSH137a00.24_.gz']; // Adicione seus arquivos aqui
     $lenght = count($files);
